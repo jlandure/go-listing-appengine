87e0dd751bfab6060e950a488022bf58aa875291
Julien Landuré <julien.landure@gmail.com>
2016-12-29 15:04:34 +0100

    feat(listing): first refactoring

--- listing/listing.go
+++ listing/listing.go
@@ -2,11 +2,13 @@ package listing
 
 import (
 	"fmt"
+	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/urlfetch"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -15,21 +17,26 @@ func init() {
 }
 
 func getServiceListing(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!\n")
 	ctx := appengine.NewContext(r)
-	transport := &oauth2.Transport{
-		Source: google.AppEngineTokenSource(ctx, "https://www.googleapis.com/auth/cloud-platform.read-only"),
-		Base:   &urlfetch.Transport{Context: ctx},
-	}
-	client := &http.Client{Transport: transport}
-	resp, err := client.Get("https://appengine.googleapis.com/v1/apps/zen-formations/services")
+	resp, err := getClientWithOAuthContext(ctx).Get("https://appengine.googleapis.com/v1/apps/zen-formations/services")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	log.Printf("HTTP GET returned status %v", resp.Status)
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Fprintf(w, "HTTP GET returned status %v", resp.Status)
-	fmt.Fprintf(w, "\n%s", body)
+	transformAndDisplay(body, w)
+}
 
+func getClientWithOAuthContext(ctx context.Context) *http.Client {
+	transport := &oauth2.Transport{
+		Source: google.AppEngineTokenSource(ctx, "https://www.googleapis.com/auth/cloud-platform.read-only"),
+		Base:   &urlfetch.Transport{Context: ctx},
+	}
+	return &http.Client{Transport: transport}
+}
+
+func transformAndDisplay(body []byte, w http.ResponseWriter) {
+	fmt.Fprintf(w, "%s", body)
 }

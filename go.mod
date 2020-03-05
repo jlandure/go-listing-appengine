54b085ae27290c55f88bf0505da8117199aa749a
Julien Landuré <julien.landure@gmail.com>
2020-03-05 23:52:48 +0100

    :sparkles: update to go113 runtime

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/jlandure/go-listing-appengine
+
+go 1.14
+
+require (
+	cloud.google.com/go v0.53.0 // indirect
+	github.com/golang/protobuf v1.3.4 // indirect
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
+	google.golang.org/api v0.20.0
+	google.golang.org/genproto v0.0.0-20200303153909-beee998c1893 // indirect
+)
--- listing.go
+++ listing.go
@@ -1,81 +1,80 @@
-package listing
+package main
 
 import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/urlfetch"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
+	"google.golang.org/api/appengine/v1"
 )
 
-func init() {
-	http.HandleFunc("/api/services", getServiceListingApi)
-	http.HandleFunc("/", getServiceListingHtml)
+func main() {
+	http.HandleFunc("/api/services", getServiceListingAPI)
+	http.HandleFunc("/", getServiceListingHTML)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func getServiceListing(w http.ResponseWriter, r *http.Request) ([]byte, error) {
+func getServiceListing(w http.ResponseWriter, r *http.Request) (appEngineServicesStruct, error) {
+	appName := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	log.Println("appName:"+appName)
 	// add CORS headers
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	ctx := appengine.NewContext(r)
-	resp, err := getClientWithOAuthContext(ctx).Get("https://appengine.googleapis.com/v1/apps/zen-formations/services")
+	ctx := context.Background()
+	appengineService, err := appengine.NewService(ctx)
+	var appEngineServices appEngineServicesStruct
+	response, err := appengine.NewAppsServicesService(appengineService).List(appName).Do()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return nil, err
+		log.Print("err calling NewAppsServicesService.List", err)
+		return appEngineServices, err
 	}
-	log.Printf("HTTP GET returned status %v", resp.Status)
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	return body, nil
+	log.Println("Loading JSON")
+	appEngineServices = transform(response.Services)
+	return appEngineServices, nil
 }
 
-func getServiceListingApi(w http.ResponseWriter, r *http.Request) {
-	body, err := getServiceListing(w, r)
+func getServiceListingAPI(w http.ResponseWriter, r *http.Request) {
+	appEngineServices, err := getServiceListing(w, r)
 	if err != nil {
 		return
 	}
-	var services AppEngineServices
-	log.Println("construction du json")
-	err = json.Unmarshal(body, &services)
-	services = transform(services)
-	data, err := json.Marshal(services)
+	data, err := json.Marshal(appEngineServices)
 	if err != nil {
 		log.Print("err mashal", err)
 	}
 	fmt.Fprintf(w, "%s", data)
 }
 
-func getServiceListingHtml(w http.ResponseWriter, r *http.Request) {
-	body, err := getServiceListing(w, r)
+func getServiceListingHTML(w http.ResponseWriter, r *http.Request) {
+	appEngineServices, err := getServiceListing(w, r)
 	if err != nil {
 		return
 	}
-	transformAndDisplay(body, w)
+	transformAndDisplay(appEngineServices, w)
 }
 
-func getClientWithOAuthContext(ctx context.Context) *http.Client {
-	transport := &oauth2.Transport{
-		Source: google.AppEngineTokenSource(ctx, "https://www.googleapis.com/auth/cloud-platform.read-only"),
-		Base:   &urlfetch.Transport{Context: ctx},
-	}
-	return &http.Client{Transport: transport}
+type appEngineServicesStruct struct {
+	Services []appEngineServiceStruct `json:"services"`
 }
 
-type AppEngineServices struct {
-	Services []AppEngineService `json:"services"`
-}
-
-type AppEngineService struct {
-	Id    string `json:"id"`
+type appEngineServiceStruct struct {
+	ID    string `json:"id"`
 	Name  string `json:"name"`
-	Url   string `json:"url"`
+	URL   string `json:"url"`
 	Title string `json:"title"`
 }
 
@@ -100,28 +99,24 @@ func formatName(name string) string {
 	return formattedName
 }
 
-func transform(services AppEngineServices) AppEngineServices {
-	for index := range services.Services {
-		services.Services[index].Title = formatName(services.Services[index].Id)
-		log.Printf("%s", services.Services[index].Title)
-		services.Services[index].Url = fmt.Sprintf("https://%s-dot-zen-formations.appspot.com/", services.Services[index].Id)
-		// 	fmt.Fprintf(w, "<li>%s</li>", services.Services[index].Id)
+func transform(services []*appengine.Service) appEngineServicesStruct {
+	appEngineServices := appEngineServicesStruct{}
+	for index := range services {
+		appEngineService := appEngineServiceStruct{
+			ID: services[index].Id,
+			Name: services[index].Name,
+			URL: fmt.Sprintf("https://%s-dot-zen-formations.appspot.com/", services[index].Id),
+			Title: formatName(services[index].Id),
+		}
+		appEngineServices.Services = append(appEngineServices.Services, appEngineService)
 	}
-	return services
+	return appEngineServices
 }
 
-func transformAndDisplay(body []byte, w http.ResponseWriter) {
-	var services AppEngineServices
-	log.Println("construction du json")
-	err := json.Unmarshal(body, &services)
-	if err != nil {
-		log.Print("err2", err)
-	}
-	services = transform(services)
+func transformAndDisplay(appEngineServices appEngineServicesStruct, w http.ResponseWriter) {
 	tmpl := template.Must(template.ParseFiles("template/listing.gohtml"))
 	tc := make(map[string]interface{})
-	tc["Services"] = services.Services
-
+	tc["Services"] = appEngineServices.Services
 	if err := tmpl.Execute(w, tc); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
